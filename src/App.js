f2cc609317182285d343ee5a150c89d2dfdddc0c
amosnyuangar <amosnyuangar@gmail.com>
2020-07-21 15:40:39 -0500

    install react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  {/* assignment 1: search all html tags */}
+     <h1>HELLO WORLD</h1>
+    </div>
+  );
+}
+
+export default App;
